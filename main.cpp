73bac0a34847f5b70b85854689e1178376305b36
StanislavGarber <stasbvk@gmail.com>
2022-04-01 23:41:50 +0300

    age, size

--- main.cpp
+++ main.cpp
@@ -10,13 +10,23 @@
 
 using namespace std;
 
-// races data
+// data
 
 vector<string> races = {"dwarf", "elf", "halfing", "human", "dragonborn", "gnome", "half-elf", "half-ork", "tiefling"};
 vector<string> dwarf_subraces = {"hill dwarf", "mountain dwarf"};
 vector<string> elf_subraces = {"high elf", "dark elf", "wood elf"};
 vector<string> human_subraces = {"damaran", "illuskan", "mulan", "rashemi", "calishite", "chondatan", "shou", "tethyrian", "turami"};
 vector<string> gnome_subraces = {"forest gnome", "rock gnome"};
+
+
+enum class Dwarf {
+  min_age = 16,
+  adult_age = 50,
+  max_age = 350,
+  min_height = 4,
+  max_height = 5,
+  average_weight = 150,
+};
 
 
 // random utils
@@ -56,21 +66,31 @@ class Character
 private:
     string race;
     string subrace;
+    int age;
+    float height;
+    float weight;
 public:
 
     Character();
 
     string get_race() { return race; };
     string get_subrace() { return subrace; };
+    int get_age() { return age; };
+    int get_height() { return height; };
+    int get_weight() { return weight; };
 
     void set_random_race();
     void set_random_subrace();
+    void set_random_age();
+    void set_random_size();
 };
 
 
 Character::Character() {
     set_random_race();
     set_random_subrace();
+    set_random_age();
+    set_random_size();
 }
 
 void Character::set_random_race() {
@@ -84,6 +104,18 @@ void Character::set_random_subrace() {
     else if(race == "human") subrace = human_subraces[::random_index(generator, human_subraces.size())];
     else if(race == "gnome") subrace = gnome_subraces[::random_index(generator, gnome_subraces.size())];
     else subrace = race;
+}
+
+void Character::set_random_age() {
+    if(race == "dwarf") age = ::random_int(generator, int(Dwarf::min_age), int(Dwarf::max_age));
+
+}
+
+void Character::set_random_size() {
+    if(race == "dwarf") {
+        height = ::random_float(generator, int(Dwarf::min_height), int(Dwarf::max_height));
+
+    }
 }
 
 
